f60d2a1a64e7d3e124324e24f953079a6344c322
Srinivashathwar <sreenivas.hatwar@accionlabs.com>
2022-10-28 14:44:38 +0530

    add node mode jo

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ const handlebars = require("handlebars");
 const exphbs = require("express-handlebars");
 const bodyparser = require("body-parser");
 const path = require("path");
-const routes = require("./backend/routes/per_route");
-require("./backend/config/per_db");
+const routes = require("./src/backend/routes/per_route");
+require("./src/backend/config/per_db");
 
 const cors = require('cors');
 
@@ -19,16 +19,7 @@ app.use(
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyparser.json());
-app.set("views", path.join(__dirname, "/frontend/"));
-app.engine(
-  "hbs",
-  exphbs.engine({
-    extname: "hbs",
-    defaultLayout: false,
-    layoutsDir: __dirname + "/frontend/",
-  })
-);
-app.set("view engine", "hbs");
+
 
 app.use(cors({
   origin: "*"
